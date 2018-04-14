2d6235555f07a2e843a4f23e4bb0866d9f7bab4d
Rafalsky <rafbe@ymail.com>
2018-04-14 15:25:31 +0200

    additional mobile navigation setupu

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 export const PrivateHeader = (props) => {
+  const navImgSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
   return (
     <div className="header">
       <div className="header__content">
+        <img className="header__nav-toggle" onClick={props.handleNavToggle} src={navImgSrc} alt="menu bars" />
         <h1 className="header__title">{props.title}</h1>
         <button className="button button--link-text" onClick={() => props.handleLougout()}>Logout</button>
       </div>
@@ -15,12 +18,16 @@ export const PrivateHeader = (props) => {
 
 PrivateHeader.propTypes = {
   title: React.PropTypes.string.isRequired,
-  handleLougout: React.PropTypes.func.isRequired
+  handleLougout: React.PropTypes.func.isRequired,
+  handleNavToggle: React.PropTypes.func.isRequired,
+  isNavOpen: React.PropTypes.bool.isRequired
 };
 
 export default createContainer(() => {
   return {
-    handleLougout: () => Accounts.logout()
+    handleNavToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+    handleLougout: () => Accounts.logout(),
+    isNavOpen: Session.get('isNavOpen')
   };
 }, PrivateHeader);
 
--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -8,13 +8,13 @@ if (Meteor.isClient) {
   describe('PrivateHeader', function () {
     it('should use title props as H1 text', function () {
       const title = 'Notes App';
-      const wrapper = mount(<PrivateHeader title={title} handleLougout={() => {}} />);
+      const wrapper = mount(<PrivateHeader isNavOpen={false} title={title} handleLougout={() => {}} />);
       expect(wrapper.find('h1').text()).toEqual(title);
     });
 
     it('should call handle logout on click', function () {
       const spy = expect.createSpy();
-      const wrapper = mount(<PrivateHeader title="" handleLougout={spy} />);
+      const wrapper = mount(<PrivateHeader isNavOpen={false} title="" handleLougout={spy} />);
       wrapper.find('button').simulate('click');
       expect(spy).toHaveBeenCalled();
     });
